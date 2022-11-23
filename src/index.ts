422512d0b66eb1e19c2f1ee10aa2ef3b3da6d752
Kohei Tanaka <kohei69@gmail.com>
2022-11-23 14:43:52 +0900

    add Mutation

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,40 @@ import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 
 const typeDefs = `
+  type Photo {
+    id: ID!
+    url: String!
+    name: String!
+    description: String
+  }
   type Query {
     totalPhotos: Int!
+    allPhotos: [Photo!]!
+  }
+  type Mutation {
+    postPhoto(name: String! description: String): Photo!
   }
 `
+let _id = 0
+const photos = []
 
 const resolvers = {
   Query: {
-    totalPhotos: () => 42
+    totalPhotos: () => photos.length,
+    allPhotos: () => photos
+  },
+  Mutation: {
+    postPhoto(parent, args) {
+      const newPhoto = {
+        id: _id++,
+        ...args
+      }
+      photos.push(args)
+      return newPhoto
+    }
+  },
+  Photo: {
+    url: parent => `http://yoursite.com/img/${parent.id}.jpg`
   }
 }
 
