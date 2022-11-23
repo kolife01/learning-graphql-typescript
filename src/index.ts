4ec931743994656cfe1ea66b5e102dd700d7e300
Kohei Tanaka <kohei69@gmail.com>
2022-11-23 12:34:59 +0900

    add resolvers

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+
+const typeDefs = `
+  type Query {
+    totalPhotos: Int!
+  }
+`
+
+const resolvers = {
+  Query: {
+    totalPhotos: () => 42
+  }
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log(`Server ready at: ${url}`);
