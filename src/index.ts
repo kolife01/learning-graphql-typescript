fa30851b6cca95215fc4bc8b45a19ea715f21a78
Kohei Tanaka <kohei69@gmail.com>
2022-11-27 16:32:02 +0900

    add taggedUsers

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ const typeDefs = `
     name: String
     avatar: String
     postedPhotos: [Photo!]!
+    inPhotos: [Photo!]!
   }
   type Photo {
     id: ID!
@@ -14,6 +15,7 @@ const typeDefs = `
     name: String!
     description: String
     postedBy: User!
+    taggedUsers: [User!]!
   }
   type Query {
     totalPhotos: Int!
@@ -25,7 +27,9 @@ const typeDefs = `
 `
 let _id = 0
 const users = [
-  {githubLogin: "tttt", name: 'Mike'}
+  {githubLogin: "mHattrup", name: 'Mike'},
+  {githubLogin: "gPlake", name: 'Glen'},
+  {githubLogin: "sSchmidt", name: 'Scor'}
 ]
 const photos = [
   {
@@ -33,8 +37,29 @@ const photos = [
     name: 'test photos',
     description: 'test d',
     category: 'ACTION',
-    githubUser: 'tttt'
+    githubUser: 'gPlake'
+  },
+  {
+    id: 2,
+    name: 'test photos2',
+    description: 'test d2',
+    category: 'ACTION',
+    githubUser: 'sSchmidt'
+  },
+  {
+    id: 3,
+    name: 'test photos3',
+    description: 'test d3',
+    category: 'ACTION',
+    githubUser: 'sSchmidt'
   }
+]
+
+const tags = [
+  { photoID: 1, userID: 'gPlake'},
+  { photoID: 2, userID: 'sSchmidt'},
+  { photoID: 2, userID: 'mHattrup'},
+  { photoID: 2, userID: 'gPlake'},
 ]
 
 const resolvers = {
@@ -57,11 +82,17 @@ const resolvers = {
     postedBy: parent => {
       return users.find(u => u.githubLogin === parent.githubUser)
     },
+    taggedUsers: parent => tags.filter(tag => tag.photoID === parent.id)
+                                .map(tag => tag.userID)
+                                .map(userID => users.find(u => u.githubLogin === userID))
   },
   User: {
     postedPhotos: parent => {
       return photos.filter(p => p.githubUser === parent.githubLogin)
-    }
+    },
+    inPhotos: parent => tags.filter(tag => tag.userID === parent.id)
+                            .map(tag => tag.photoID)
+                            .map(photoID => photos.find(p => p.id === photoID))
   }
 }
 
